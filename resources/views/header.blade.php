726ec5ff2fe3119b78db5e2a4617972a593d6feb
= <=>
2020-05-23 20:03:01 +0100

    Created css file

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,35 +1,3 @@
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-    }
-
-    .header {
-        background: hsl(0, 50%, 50%);
-        height: 50px;
-    }
-
-    .header ul {
-        margin: 0;
-        padding: 0;
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        padding: 0 30px;
-    }
-
-    .header li {
-        margin: 0 15px 0 0;
-        padding: 0;
-        display: block;
-    }
-
-    .header a {
-        color: #fff;
-    }
-</style>
-
 <header class="header">
     <ul>
         <li><a href="/">Home</a></li>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/main.css">
 </head>
 <body>
     @include('header')
